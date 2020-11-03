d81b6d57baff931c31b47d98fff565dff8afdc87
unknown <wilmerpelico22@gmail.com>
2020-11-03 01:49:47 -0600

    Inicio de generación de turnos

--- src/java/lib/TurnoConsultas.java
+++ src/java/lib/TurnoConsultas.java
@@ -25,21 +25,31 @@ public class TurnoConsultas extends Conexion {
         int numerodeTurno;
         int resultUpdate=0;
         String codigo = null;
+        this.conexion = getConexion();
 
         try{
+            //Si el cliente eligió un servicio
             if (tipoServicio.equals("servicio")){
+                //Se cuenta la cantidad de servicios solicitdos
                 String cantidadTurnos = "SELECT COUNT(*) AS total FROM `turno` WHERE tipo_de_servicio= ?";
-                pstm = getConexion().prepareCall(cantidadTurnos);
+                //Se prepara la consulta
+                pstm = this.conexion.prepareCall(cantidadTurnos);
+                //Se le da el valor a la variable temporal de la consulta, con el valor recibido del formulario
                 this.pstm.setString(1, tipoServicio);
+                //Ejecutamos la consulta
                 this.rs = this.pstm.executeQuery();
+                //Recorremos en este caso su única columna 
                 rs.next();
+                //Le asignamos el valor a nuestra variable contador
                 numerodeTurno = this.rs.getInt("total");
+                //Mensajes en consola para el Test
                 System.out.println("======== RESULTADO DE LA CONSULTA SERVICIO ========");
                 System.out.println(numerodeTurno);
+                //Retornamos true si no falla nada, para nuestro test
                 return true;
             } else if (tipoServicio.equals("producto")){
                 String cantidadTurnos = "SELECT COUNT(*) AS total FROM `turno` WHERE tipo_de_servicio= ?";
-                pstm = getConexion().prepareCall(cantidadTurnos);
+                pstm = this.conexion.prepareCall(cantidadTurnos);
                 this.pstm.setString(1, tipoServicio);
                 rs = this.pstm.executeQuery();
                 rs.next();
